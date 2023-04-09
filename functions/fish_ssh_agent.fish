b7a0f17efa2f3eb52a61e7e162bbd5b3ff72fa1b
may <m4rch3n1ng@gmail.com>
2023-04-09 18:24:15 +0200

    add manual mode, fix issues

--- functions/fish_ssh_agent.fish
+++ functions/fish_ssh_agent.fish
@@ -1,20 +1,23 @@
 # adapted from https://stackoverflow.com/a/18915067
 set SSH_ENV "$HOME/.ssh/agent-environment"
 
-function start_agent
+function start_agent -d "start ssh-agent"
 	/usr/bin/ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
 	chmod 600 $SSH_ENV
 	. $SSH_ENV > /dev/null
-	/usr/bin/ssh-add
 end
 
-function fish_ssh_agent --description "Start ssh-agent if not started yet, or uses already started ssh-agent."
+function fish_ssh_agent -d "use already started ssh-agent or start a new one"
+	argparse "m/manual" -- $argv
+
 	if test -f "$SSH_ENV"
 		. "$SSH_ENV" > /dev/null
 		if not ps -ef | grep $SSH_AGENT_PID | grep "ssh-agent -c\$" &> /dev/null
 			start_agent
+			[ ! $_flag_m ] && /usr/bin/ssh-add
 		end
 	else
 		start_agent
+		[ ! $_flag_m ] && /usr/bin/ssh-add
 	end
 end
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # ssh agent plugin for fish
 
-a plugin for launching the ssh-agent and keeping
+a fish shell plugin for launching the ssh-agent and keeping it running
 
 ## install
 
@@ -16,6 +16,13 @@ and add this line to the end of your [config.fish](https://fishshell.com/docs/cu
 fish_ssh_agent
 ```
 
+if you don't want to automatically start the ssh agent on shell startup
+but instead want to manually start the ssh-agent using `$ ssh-add` add `--manual` as an argument
+
+```sh
+fish_ssh_agent --manual
+```
+
 ## info
 
-this is modeled after [this stackoverflow answer](https://stackoverflow.com/a/18915067), but converted to be used within fish.
+this is modeled after [this stackoverflow answer](https://stackoverflow.com/a/18915067), but converted to be used with fish.
